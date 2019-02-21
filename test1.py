9eebcc4ce63fe9a8258db4f9c3d342f4d6659b87
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-02-21 14:01:35 -0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,1 @@
-a = {'SSW567-Testing-HW01': 2, 'SSW567-Testing-HW02a': 10, 'SSW567-Testing-HW04a-API': 20}
+a = {'hellogitworld': 30, 'helloworld': 6, 'Mocks': 9, 'Project1': 2, 'threads-of-life': 1}
