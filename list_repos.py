1cd3305b825b4e405b9177a8e6f4be9ac23bdfe7
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-02-21 14:47:12 -0500

    Delete list_repos.py

--- list_repos.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-
-print("Enter the name of the GitHub user: ")
-username = input()
-
-repo = requests.get('https://api.github.com/users/%s/repos' % username).json()
-for element in repo:
-    name = element['name']
-    print(name)
