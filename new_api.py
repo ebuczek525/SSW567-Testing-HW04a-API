6558e1e104c90b06f1085df70100ed447bac09d7
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-03-05 14:48:24 -0500

    Update new_api.py

--- new_api.py
+++ new_api.py
@@ -1,1 +1,47 @@
+import requests
+import json
+import unittest
 
+
+def apiFunction(username):
+    repos = getRepos(username)
+    dict = {}
+    for repo in repos:
+        reponame = repo["name"]
+        dict[reponame] = getCommits(username, reponame)
+
+    return dict
+
+def getRepos(username):
+    r = requests.get('https://api.github.com/users/' + username + '/repos');
+    data = r.json()  
+    return data
+
+def getCommits(username, reponame):
+    r = requests.get('https://api.github.com/repos/' + username + '/' + reponame + '/commits');
+    data = r.json()
+    return len(data)
+
+
+class TestapiFunction(unittest.TestCase):
+    @patch('requests.get')
+
+    def test_apiFunction(self, mockedReq):
+        mockedReq.return_value = MockResponse('{'hellogitworld': 30, 'helloworld': 6, 'Mocks': 9, 'Project1': 2, 'threads-of-life': 1}')      
+        api = apiFunction(self.username)
+        self.assertGreater(len(apiFunction), 0)
+
+    def test_getRepos(self, mockedReq):
+        mockedReq.return_value = MockResponse('hellogitworld, helloworld, Mocks, Project1, threads-of-life')      
+        repos = getRepos(self.username)
+        self.assertGreater(len(getRepos), 0)
+
+    def test_getCommits(self, mockedReq):
+        mockedReq.return_value = MockResponse('30')      
+        commits = getCommits(self.username, self.reponame)
+        self.assertGreater(len(getCommits), 0)
+
+
+if __name__ == "__main__":
+    print('Running unit tests')
+    unittest.main()
