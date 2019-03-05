3814e59102f9a2e931aca54d53ece7e0d532dfad
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-03-05 15:49:51 -0500

    Update new_api.py

--- new_api.py
+++ new_api.py
@@ -13,12 +13,12 @@ def apiFunction(username):
     return dict
 
 def getRepos(username):
-    r = requests.get('https://api.github.com/users/' + str(username) + '/repos')
+    r = requests.get('https://api.github.com/users/' + username + '/repos')
     data = r.json()
     return data
 
 def getCommits(username, reponame):
-    r = requests.get('https://api.github.com/repos/' + str(username) + '/' + str(reponame) + '/commits')
+    r = requests.get('https://api.github.com/repos/' + username + '/' + reponame + '/commits')
     data = r.json()
     return len(data)
 
@@ -28,19 +28,19 @@ class TestapiFunction(unittest.TestCase):
     @mock.patch('requests.get')
     def test_apiFunction(self, mockedReq):
         mockedReq.return_value = 'hellogitworld: 30, helloworld: 6, Mocks: 9, Project1: 2, threads-of-life: 1'
-        api = apiFunction(self)
+        api = apiFunction()
         self.assertGreater(len(api), 0)
 
     @mock.patch('requests.get')
     def test_getRepos(self, mockedReq):
         mockedReq.return_value = 'hellogitworld, helloworld, Mocks, Project1, threads-of-life'
-        repos = getRepos(self)
+        repos = getRepos()
         self.assertGreater(len(repos), 0)
     
     @mock.patch('requests.get')
     def test_getCommits(self, mockedReq):
         mockedReq.return_value = '30'
-        commits = getCommits(self, self)
+        commits = getCommits()
         self.assertGreater(len(commits), 0)
 
 
