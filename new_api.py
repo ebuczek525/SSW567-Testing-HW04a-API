5cc5b6fea5256b3a86362175494d0a30fa92bacb
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-03-06 18:32:56 -0500

    Update new_api.py

--- new_api.py
+++ new_api.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 import requests
-from unittest.mock import patch
+from unittest.mock import MagicMock
 
 def apiFunction(username):
     repos = getRepos(username)
@@ -25,19 +25,20 @@ def getCommits(username, reponame):
 
 class TestapiFunction(unittest.TestCase):
 
-    def test_apiFunction(self, mocked):
-        mocked.side_effect = '[hellogitworld: 30, helloworld: 6, Mocks: 9, Project1: 2, threads-of-life: 1]'
-        self.assertEqual(mocked.side_effect, [
-        hellogitworld: 30, helloworld: 6, Mocks: 9, Project1: 2, threads-of-life: 1])
+    def test_apiFunction(self):
+        mock = MagicMock()
+        mock.__str__.return_value = '[hellogitworld: 30, helloworld: 6, Mocks: 9, Project1: 2, threads-of-life: 1]'
+        self.assertEqual(str(mock), [hellogitworld: 30, helloworld: 6, Mocks: 9, Project1: 2, threads-of-life: 1])
 
-    def test_getRepos(self, mocked):
-        mocked.side_effect = '[hellogitworld, helloworld, Mocks, Project1, threads-of-life]'
-        self.assertEqual(mocked.side_effect, [
-        hellogitworld, helloworld, Mocks, Project1, threads-of-life])
+    def test_getRepos(self):
+        mock = MagicMock()
+        mock.__str__.return_value = '[hellogitworld, helloworld, Mocks, Project1, threads-of-life]'
+        self.assertEqual(str(mock), [hellogitworld, helloworld, Mocks, Project1, threads-of-life])
     
-    def test_getCommits(self, mocked):
-        mocked.side_effect = '30'
-        self.assertEqual(mocked.side_effect, 30)
+    def test_getCommits(self):
+        mock = MagicMock()
+        mock.__str__.return_value = '30'
+        self.assertEqual(str(mock), 30)
 
 
 if __name__ == "__main__":
